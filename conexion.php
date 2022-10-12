ef8715fb6dcfd6995115815543a204a29e35e34a
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-10-12 00:43:57 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 $db_host = "157.90.91.29";
 $db_user = "agenciae";
 $db_pass = "q294g.#PwM9SYy";
-$db_name = "agenciae_coardecom";
+$db_name = "agenciae_coardecom"; //nombre de la base de datos
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 mysqli_set_charset($con, 'utf8'); //Muy importante esta linea, guardara el contenido que contenga acentos de manera correcta configurando la bd con el UTF-8 spanis ci
